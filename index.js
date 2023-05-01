741692e84ba3b81806c5a63db7975956fbd6ba99
Antoine Kingue <etorefr@gmail.com>
2023-05-02 01:20:08 +0200

    🎨 add errors catcher

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// @ts-check
-const app = require("./src/app");
+const main = async () => {
+  const app = require("./src/app");
 
-app.listen(8080, () => console.log("Listening on port 8080"));
+  app.listen(8080, () => console.log("Listening on port 8080"));
+};
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
